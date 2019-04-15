86f4e1c7bf5aed09164fd2c966fd14eef4676603
Asif Eqbal <asif.eqbal@vrisini.com>
2019-04-15 16:11:29 +0530

    Initial commit

---
+++ src/components/AddItem.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import {
+	  StyleSheet,
+	  View,	
+    TextInput,
+    Button,
+    Text
+} from 'react-native';
+
+import {db} from '../config';
+
+//add item to firebase db
+let addItem =(item)=> {
+  db.ref('/items').push({
+    itemInfo:item
+  });
+};
+
+//get items from firebase db
+let itemRef = db.ref('/items');
+
+
+export default class AddItem extends Component {
+	constructor(props) {
+	  super(props);
+	 this.state = {
+     itemInfo:'',
+     items:[],
+   }
+   this.handelerChange = this.handelerChange.bind(this);
+   this.clickToAdd = this.clickToAdd.bind(this);
+  }
+  
+  clickToAdd(){
+   addItem(this.state.itemInfo);
+  }
+  handelerChange(e){
+    this.setState({itemInfo:e.nativeEvent.text})
+  }
+  componentDidMount(){
+    itemRef.on('value',myDataTable =>{
+      let data = myDataTable.val();
+      let items = Object.values(data);
+      this.setState({items});
+    });
+  }
+	
+    render() {
+    	
+        return (
+            <View>
+           <TextInput 
+           style={{height: 40, borderColor: 'gray', borderWidth: 1}}          
+              onChange={this.handelerChange}
+              value={this.state.itemInfo}              
+            ></TextInput>
+            <Button
+            onPress={this.clickToAdd}
+            title="Add Item"
+            color="#2B64C7"
+            >
+            </Button>
+            {this.state.items.map( x => {
+              return <Text>{x.itemInfo}</Text>
+
+            })
+            }
+
+            </View>           
+        );
+    }
+}
